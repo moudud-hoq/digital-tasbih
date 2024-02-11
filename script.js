1f99afa67fd3026ccc395fcb4c2ee56046e98578
moududulhaque <moududulhaque@gmail.com>
2024-02-11 22:55:18 +0600

    subhanAllahDisplay.innerText = subhanAllahInitialValue;

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const subhanAllahDisplay = document.getElementById('subhanAllahDisplay');
-const subhanAllahIncrimentBtn = document.getElementById('subhanAllahDecrimentBtn');
+const subhanAllahIncrimentBtn = document.getElementById('subhanAllahIncrimentBtn');
 const subhanAllahDecrimentBtn = document.getElementById('subhanAllahDecrimentBtn');
 
 const alhamdulillahDisplay = document.getElementById('alhamdulillahDisplay');
-const alhamdulillahIncrimentBtn = document.getElementById('alhamdulillahDecrimentBtn');
+const alhamdulillahIncrimentBtn = document.getElementById('alhamdulillahIncrimentBtn');
 const alhamdulillahDecrimentBtn = document.getElementById('alhamdulillahDecrimentBtn');
 
 
 const allahAkberDisplay = document.getElementById('allahAkberDisplay');
-const allahAkberIncrimentBtn = document.getElementById('allahAkberDecrimentBtn');
+const allahAkberIncrimentBtn = document.getElementById('allahAkberIncrimentBtn');
 const allahAkberDecrimentBtn = document.getElementById('allahAkberDecrimentBtn');
 
 const resetBtn = document.getElementById('resetBtn');
@@ -19,6 +19,8 @@ let alhamdulillahInitialValue = 0;
 let allahAkberInitialValue = 0;
 
 //execution by eventListener
+//এই ফাংশনের মধ্যে আমরা যা এক্সিকিউট করতে চাইবো ইউজার বাটনে ক্লিক করার মাধ্যমে তাই সামনে চলে আসবে। তার মানে এই ফাংশনেই কন্ডিশন, লুপ, সহ যা যা আছে চলবে।
 subhanAllahIncrimentBtn.addEventListener('click', function () {
-
+    subhanAllahInitialValue += 1;
+    subhanAllahDisplay.innerText = subhanAllahInitialValue;
 })
