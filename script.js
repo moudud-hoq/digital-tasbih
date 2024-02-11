2291b1cac179ef2e97c2f1ce3a119c53d91834f6
moududulhaque <moududulhaque@gmail.com>
2024-02-11 23:02:27 +0600

    SubhanAllah,Alhamdulillah,Allauakbar button increament annd decreament done

--- script.js
+++ script.js
@@ -20,7 +20,28 @@ let allahAkberInitialValue = 0;
 
 //execution by eventListener
 //এই ফাংশনের মধ্যে আমরা যা এক্সিকিউট করতে চাইবো ইউজার বাটনে ক্লিক করার মাধ্যমে তাই সামনে চলে আসবে। তার মানে এই ফাংশনেই কন্ডিশন, লুপ, সহ যা যা আছে চলবে।
+
+//SubhanAllah Button
 subhanAllahIncrimentBtn.addEventListener('click', function () {
     subhanAllahInitialValue += 1;
     subhanAllahDisplay.innerText = subhanAllahInitialValue;
+})
+
+subhanAllahDecrimentBtn.addEventListener('click', function () {
+    subhanAllahInitialValue -= 1;
+    subhanAllahDisplay.innerText = subhanAllahInitialValue;
+})
+
+
+//Alhamdulillah Button
+alhamdulillahIncrimentBtn.addEventListener('click', function () {
+    alhamdulillahInitialValue += 1;
+    alhamdulillahDisplay.innerText = alhamdulillahInitialValue;
+})
+
+
+//Allahuakbar Button
+allahAkberIncrimentBtn.addEventListener('click', function () {
+    allahAkberInitialValue += 1;
+    allahAkberDisplay.innerText = allahAkberInitialValue;
 })
