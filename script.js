a098738c8037665c96949669171cb9a755eec8e7
moududulhaque <moududulhaque@gmail.com>
2024-02-11 22:43:56 +0600

    Each element Getting By ID

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 const subhanAllahDisplay = document.getElementById('subhanAllahDisplay');
+const subhanAllahIncrimentBtn = document.getElementById('subhanAllahDecrimentBtn');
+const subhanAllahDecrimentBtn = document.getElementById('subhanAllahDecrimentBtn');
+
+const alhamdulillahDisplay = document.getElementById('alhamdulillahDisplay');
+const alhamdulillahIncrimentBtn = document.getElementById('alhamdulillahDecrimentBtn');
+const alhamdulillahDecrimentBtn = document.getElementById('alhamdulillahDecrimentBtn');
+
+
+const allahAkberDisplay = document.getElementById('allahAkberDisplay');
+const allahAkberIncrimentBtn = document.getElementById('allahAkberDecrimentBtn');
+const allahAkberDecrimentBtn = document.getElementById('allahAkberDecrimentBtn');
+
+const resetBtn = document.getElementById('resetBtn');
+
+//Now initializing value
+let subhanAllahInitialValue = 0;
+let alhamdulillahInitialValue = 0;
+let allahAkberInitialValue = 0;
+
+//execution by eventListener
+subhanAllahIncrimentBtn.addEventListener('click', function () {
+
+})
